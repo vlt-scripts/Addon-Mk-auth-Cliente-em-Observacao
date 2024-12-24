a34f13a14109b3ac10167ffae41c185f8abcf7e4
vlt-scripts <valteir.034@gmail.com>
2024-12-24 09:25:20 -0400

    Melhorias
    Adicionado biblioteca personalizada, para se adequar melhor ao layout mk-auth.

--- rel_cli_obs/index.php
+++ rel_cli_obs/index.php
@@ -56,7 +56,7 @@ if ($result) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 	
 	<!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
